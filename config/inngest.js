b08a125af1b1b31f7a70bb16f041f430b8f21bac
Sihir7 <158707214+Sihir7@users.noreply.github.com>
2025-04-25 19:13:12 +0530

    Fix inngest integration and build errors

--- config/inngest.js
+++ config/inngest.js
@@ -11,12 +11,12 @@ id:'sync-user-from-clerk'
 },
 {event:'clerk/user.created'},
 async(event)=>{
-const {id,first_name,last_name,email_address,image_url}= event.data
+const {id,first_name,last_name,email_addresses,image_url}= event.data
 const userData={
-    id:id,
-    email:email_adresses[0].email_adresses,
+    _id:id,
+    email: email_addresses[0].email_address,
     name:first_name+' ' + last_name,
-    imageUrl:image_Url
+    imageUrl: image_url
 }
 await connectDB()
 await User.create(userData)
@@ -31,21 +31,21 @@ export const syncUserUpdation=inngest.createFunction(
         event:'clerk/user.updated'
     },
     async({event})=>{
-        const {id,first_name,last_name,email_address,image_url}= event.data
+        const {id, first_name, last_name,email_addresses,image_url}= event.data
 const userData={
     id:id,
-    email:email_adresses[0].email_adresses,
+    email:email_addresses[0].email_address,
     name:first_name+' ' + last_name,
-    imageUrl:image_Url
+    imageUrl: image_url
 }
 await connectDB()
-await User.findByIdUpdate(id,userData)
+await User. findByIdAndUpdate(id,userData)
     }
 )
 //Inngest Function to delete user from database
 export const syncUserDeletion=inngest.createFunction(
     {
-        id:'delete-user-from-clerk'
+        id:'delete-user-with-clerk'
     },
     {
         event:'clerk/user.deleted'
@@ -53,7 +53,7 @@ export const syncUserDeletion=inngest.createFunction(
     async({event})=>{
         const{id}=event.data
         await connectDB()
-        await User.findByIdDelete(id)
+        await User.findByIdAndDelete(id)
        
 }
 )
